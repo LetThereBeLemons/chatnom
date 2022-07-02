96cb1ec9bdfe15eda154b600ff74568e58098640
LetThereBeLemons <jamsieh@icloud.com>
2022-07-02 17:46:22 +0100

    Added files

---
+++ chatnom.py
@@ -0,0 +1,3 @@
+# Chatnom - A LetThereBeLemons creat
+# Liscenced under DONT STEAL MY CODE YOU ASSHOLE (DSMCYA)
+version = "c0v1-r1"
---
+++ names.py
@@ -0,0 +1,3 @@
+# Chatnom - A LetThereBeLemons creat
+# Liscenced under DONT STEAL MY CODE YOU ASSHOLE (DSMCYA)
+from chatnom import version
